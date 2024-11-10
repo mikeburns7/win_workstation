9f8305f4f8f2affa8cf3bfe4a289a89be351d181
mike burns <mike.burns7@gmail.com>
2024-11-09 22:30:18 -0500

    add python

--- win_workstation.ps1
+++ win_workstation.ps1
@@ -2,5 +2,6 @@ winget install Amazon.AWSCLI
 winget install Hashicorp.Terraform
 winget install Git.git
 winget install Microsoft.VisualStudioCode
+winget install -e --id Python.Python.3.12 --location C:\Python312
 
 winget upgrade --all
